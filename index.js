e681d0f68a6d42ea927edaba559aed1c21fe40c3
Aron Galvan <arongalvan03@gmail.com>
2022-05-23 17:45:19 -0700

    commit tree d5883540e272df08319d0693723e70fa878c2f32

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    console.log(customerName);
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(value) {
+    bestCustomer = value;
+}
+
+const leastFavoriteCustomer = "Ana";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Frank";
+}
+
